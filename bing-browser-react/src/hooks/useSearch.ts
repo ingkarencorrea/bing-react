4d8e121ab35c913dda2811d13620231a02f8677d
Karen Michelle Correa <kmcorreaa@correo.udistrital.edu.co>
2023-05-03 23:47:40 -0500

    feat 💄 Add UI styling with Tailwind
    Remove default css files.
    Add Tailwind to the project.

--- bing-browser-react/src/hooks/useSearch.ts
+++ bing-browser-react/src/hooks/useSearch.ts
@@ -1,10 +1,16 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 export function useSearch() {
     const [search, updateSearch] = useState('')
     const [error, setError] = useState('')
+    const isFirstInput = useRef(true)
 
     useEffect(() => {
+        if (isFirstInput.current) {
+            isFirstInput.current = search === ''
+            return
+        }
+
         if (search.length < 3) {
             setError('Search term must be longer than 3 characters')
             return
---
+++ bing-browser-react/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
